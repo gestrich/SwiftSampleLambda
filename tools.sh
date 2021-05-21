80d04e921f452f056f4fcd76b3d3659a99772911
Bill Gestrich <bill@foreflight.com>
2021-05-21 07:33:11 -0400

    Updates

--- tools.sh
+++ tools.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
 function(){
-  s3Bucket="TestBucket5"; sam build --parameter-overrides UploadBucketName="${s3Bucket}"; sam deploy --stack-name Test5 --s3-bucket="${s3Bucket}";
+  #Getting error because bucket name  Resource: 'arn:aws:s3:::swift-simple-lambda-bucket/*' is still in template
+  s3Bucket="test5.gestrich.org"; sam build --parameter-overrides UploadBucketName="${s3Bucket}"; sam deploy --stack-name Test5 --parameter-overrides ParameterKey=UploadBucketName,ParameterValue="${s3Bucket}" --s3-bucket=$s3Bucket --capabilities CAPABILITY_IAM;
 }
 
 
